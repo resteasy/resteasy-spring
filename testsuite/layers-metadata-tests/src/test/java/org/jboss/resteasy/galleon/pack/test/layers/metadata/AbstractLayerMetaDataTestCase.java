e2ed79028ae50215bac0aa184a5737145ad925c8
Jean-Francois Denise <jdenise@redhat.com>
2023-10-31 14:07:12 +0100

    Add unit tests for the Galleon layer inclusion rule

---
+++ testsuite/layers-metadata-tests/src/test/java/org/jboss/resteasy/galleon/pack/test/layers/metadata/AbstractLayerMetaDataTestCase.java
@@ -0,0 +1,73 @@
+package org.jboss.resteasy.galleon.pack.test.layers.metadata;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.wildfly.glow.Arguments;
+import org.wildfly.glow.GlowMessageWriter;
+import org.wildfly.glow.GlowSession;
+import org.wildfly.glow.ScanResults;
+import org.wildfly.glow.maven.MavenResolver;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AbstractLayerMetaDataTestCase {
+
+    private static String URL_PROPERTY = "wildfly-glow-galleon-feature-packs-url";
+    private static Path ARCHIVES_PATH = Paths.get("target/glow-archives");
+
+    @BeforeClass
+    public static void prepareArchivesDirectory() throws Exception {
+        Path glowXmlPath = Path.of("target/test-classes/glow");
+        System.setProperty(URL_PROPERTY, glowXmlPath.toUri().toString());
+        if (Files.exists(ARCHIVES_PATH)) {
+            Files.walkFileTree(ARCHIVES_PATH, new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+        Files.createDirectories(ARCHIVES_PATH);
+    }
+
+
+    protected static Path createWebArchive(String archiveName, String xmlName, URL xmlContent) {
+        WebArchive war = ShrinkWrap.create(WebArchive.class);
+        war.addAsWebInfResource(xmlContent, xmlName);
+        ZipExporter exporter = war.as(ZipExporter.class);
+        Path path = ARCHIVES_PATH.resolve(archiveName);
+        exporter.exportTo(path.toFile());
+        return path;
+    }
+
+    protected Set<String> checkLayersForArchive(Path archivePath, String expectedLayer) throws Exception {
+        Arguments arguments = Arguments.scanBuilder().setBinaries(Collections.singletonList(archivePath)).build();
+        ScanResults scanResults = GlowSession.scan(MavenResolver.newMavenResolver(), arguments, GlowMessageWriter.DEFAULT);
+        Set<String> foundLayers = scanResults.getDiscoveredLayers().stream().map(l -> l.getName()).collect(Collectors.toSet());
+
+
+        Assert.assertTrue(foundLayers.contains(expectedLayer));
+
+        return foundLayers;
+    }
+}
