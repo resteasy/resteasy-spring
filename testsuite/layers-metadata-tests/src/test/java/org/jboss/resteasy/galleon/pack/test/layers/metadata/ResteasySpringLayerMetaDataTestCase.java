39950ce0ab25241c1b181865cc267c377605c580
阿男 <l.weinan@gmail.com>
2023-11-08 01:27:02 +0800

    add test for `resteasy-spring-web` layer

--- testsuite/layers-metadata-tests/src/test/java/org/jboss/resteasy/galleon/pack/test/layers/metadata/ResteasySpringLayerMetaDataTestCase.java
+++ testsuite/layers-metadata-tests/src/test/java/org/jboss/resteasy/galleon/pack/test/layers/metadata/ResteasySpringLayerMetaDataTestCase.java
@@ -1,20 +1,41 @@
 package org.jboss.resteasy.galleon.pack.test.layers.metadata;
 
-import org.junit.BeforeClass;
+import org.jboss.resteasy.utils.TestUtilSpring;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 
 import java.nio.file.Path;
 
 public class ResteasySpringLayerMetaDataTestCase extends AbstractLayerMetaDataTestCase {
     private static Path web;
+    private static Path web2;
 
-    @BeforeClass
-    public static void createArchives() {
-        web = createWebArchive("web.war", "web.xml", ResteasySpringLayerMetaDataTestCase.class.getResource("/web.xml"));
+    public static void createArchiveForResteasySpring(String webXmlPath) {
+        WebArchive war = createWebArchive("web.xml", ResteasySpringLayerMetaDataTestCase.class.getResource(webXmlPath));
+        ZipExporter exporter = war.as(ZipExporter.class);
+        web = ARCHIVES_PATH.resolve("web.war");
+        exporter.exportTo(web.toFile());
+    }
+
+    public static void createArchiveForResteasySpringWeb(String webXmlPath) {
+        WebArchive war = createWebArchive("web.xml", ResteasySpringLayerMetaDataTestCase.class.getResource(webXmlPath));
+        war.addClass(TestController.class);
+        TestUtilSpring.addSpringLibraries(war);
+        ZipExporter exporter = war.as(ZipExporter.class);
+        web2 = ARCHIVES_PATH.resolve("web2.war");
+        exporter.exportTo(web2.toFile());
     }
 
     @Test
-    public void testWeb() throws Exception {
+    public void testResteasySpring() throws Exception {
+        createArchiveForResteasySpring("/web.xml");
         checkLayersForArchive(web, "resteasy-spring");
+    }
+
+    @Test
+    public void createArchiveForResteasySpringWeb() throws Exception {
+        createArchiveForResteasySpringWeb("/web2.xml");
+        checkLayersForArchive(web2, "resteasy-spring-web");
     }
 }
---
+++ testsuite/layers-metadata-tests/src/test/java/org/jboss/resteasy/galleon/pack/test/layers/metadata/TestController.java
@@ -0,0 +1,20 @@
+package org.jboss.resteasy.galleon.pack.test.layers.metadata;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/" + TestController.CONTROLLER_PATH)
+public class TestController {
+
+    public static final String CONTROLLER_PATH = "spring";
+
+    @GetMapping("/hello")
+    public String string(@RequestParam String name) {
+        return "hello " + name;
+    }
+
+}
